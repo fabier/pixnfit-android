205a4f00cb3e53ca5d5260c93f5048d43b91f404
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-18 12:34:52 +0100

    Ajout de la page de login, avec mot de passe automatiquement renseigné dans passwords.xml

---
+++ app/src/main/java/com/pixnfit/LoginActivity.java
@@ -0,0 +1,80 @@
+package com.pixnfit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.pixnfit.ws.AuthenticateAsyncTask;
+
+import org.json.JSONObject;
+
+/**
+ * Created by fabier on 05/02/16.
+ */
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+    private EditText loginEditText;
+    private EditText passwordEditText;
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginEditText = (EditText) findViewById(R.id.loginEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+//        Button recoverPasswordButton = (Button) findViewById(R.id.recoverPasswordButton);
+
+//        recoverPasswordButton.setPaintFlags(recoverPasswordButton.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        loginButton.setOnClickListener(this);
+//        recoverPasswordButton.setOnClickListener(this);
+
+        sharedPreferences = getSharedPreferences("pixnfit", MODE_PRIVATE);
+        String login = getResources().getString(R.string.pixnfit_login);
+        String password = getResources().getString(R.string.pixnfit_password);
+        loginEditText.setText(sharedPreferences.getString("login", login));
+        passwordEditText.setText(sharedPreferences.getString("password", password));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.loginButton:
+                final String login = loginEditText.getText().toString();
+                final String password = passwordEditText.getText().toString();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("login", login);
+                editor.putString("password", password);
+                editor.commit();
+                AuthenticateAsyncTask authenticateAsyncTask = new AuthenticateAsyncTask(this) {
+                    @Override
+                    protected void onPostExecute(JSONObject result) {
+                        if (result != null) {
+                            // Store credentials for next launch
+                            Intent intent = new Intent(getApplicationContext(), GridActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Invalid login/password", Toast.LENGTH_SHORT);
+                            toast.show();
+                        }
+                    }
+                };
+                authenticateAsyncTask.execute();
+                break;
+//            case R.id.recoverPasswordButton:
+//                Intent intent = new Intent(this, RecoverPasswordActivity.class);
+//                startActivity(intent);
+//                break;
+            default:
+                break;
+        }
+    }
+}
