3d91e9c7875b364044f4da6bace0dd41ed2e451e
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-18 13:33:26 +0100

    Constantes utilisées pour les appels WS

---
+++ app/src/main/java/com/pixnfit/ws/WsConstants.java
@@ -0,0 +1,11 @@
+package com.pixnfit.ws;
+
+/**
+ * Created by fabier on 16/02/16.
+ */
+public interface WsConstants {
+
+    public static final String HOSTNAME = "pixnfit.com"; //"192.168.1.128"; // "linuxfabier"; // "192.168.1.128";
+//    public static String BASE_URL = "http://" + HOSTNAME + ":8080/pixnfit/api/v1";
+    public static String BASE_URL = "http://" + HOSTNAME + "/api/v1";
+}
