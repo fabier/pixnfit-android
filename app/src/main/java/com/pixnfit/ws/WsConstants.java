83e43083d493a1b5e8895d044bd34e40007f2a48
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-26 18:05:40 +0100

    Utilisation du serveur de prod

--- app/src/main/java/com/pixnfit/ws/WsConstants.java
+++ app/src/main/java/com/pixnfit/ws/WsConstants.java
@@ -6,9 +6,9 @@ package com.pixnfit.ws;
 public interface WsConstants {
 
     // Switch permettant de configurer pour la prod ou pour le dev local
-    boolean devMode = true;
+    boolean DEV_MODE = false;
 
     // Production Configuration
-    String HOSTNAME = devMode ? "192.168.1.128" : "pixnfit.com";
-    String BASE_URL = devMode ? "http://" + HOSTNAME + ":8080/pixnfit/api/v1" : "http://" + HOSTNAME + "/api/v1";
+    String HOSTNAME = DEV_MODE ? "192.168.1.128" : "pixnfit.com";
+    String BASE_URL = DEV_MODE ? "http://" + HOSTNAME + ":8080/pixnfit/api/v1" : "http://" + HOSTNAME + "/api/v1";
 }
