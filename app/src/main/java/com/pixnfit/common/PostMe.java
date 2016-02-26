9995eae91b84021b0f1d844d49f03b4ac910b809
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-26 16:08:39 +0100

    Ajout de la classe PostMe
    Contient des données spécifiques à un utilisateur pour un post donné

---
+++ app/src/main/java/com/pixnfit/common/PostMe.java
@@ -0,0 +1,11 @@
+package com.pixnfit.common;
+
+import java.util.List;
+
+/**
+ * Created by fabier on 26/02/16.
+ */
+public class PostMe {
+    public PostVote vote;
+    public List<PostComment> comments;
+}
