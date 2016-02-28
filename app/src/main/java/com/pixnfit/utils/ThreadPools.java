0551f4301e45fea3c7fbc5d30919c0cafd032618
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-28 22:47:55 +0100

    Ajout d'une classe utilitaire pour identifier les pools de threads utilisées dans l'application

---
+++ app/src/main/java/com/pixnfit/utils/ThreadPools.java
@@ -0,0 +1,12 @@
+package com.pixnfit.utils;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by fabier on 28/02/16.
+ */
+public class ThreadPools {
+    public static final Executor IMAGELOAD_THREADPOOL = Executors.newFixedThreadPool(4);
+    public static final Executor METADATA_THREADPOOL = Executors.newFixedThreadPool(4);
+}
