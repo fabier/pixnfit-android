0e471433e13149a8a6bf22ea17de70572bd3f99a
Pierre FABIER <pierre.fabier@gmail.com>
2016-02-28 22:48:11 +0100

    Ajout de la classe AsyncDrawable

---
+++ app/src/main/java/com/pixnfit/async/AsyncDrawable.java
@@ -0,0 +1,23 @@
+package com.pixnfit.async;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by fabier on 28/02/16.
+ */
+public class AsyncDrawable extends BitmapDrawable {
+    private final WeakReference<BitmapWorkerTask> bitmapWorkerTaskReference;
+
+    public AsyncDrawable(Resources res, Bitmap bitmap, BitmapWorkerTask bitmapWorkerTask) {
+        super(res, bitmap);
+        bitmapWorkerTaskReference = new WeakReference<BitmapWorkerTask>(bitmapWorkerTask);
+    }
+
+    public BitmapWorkerTask getBitmapWorkerTask() {
+        return bitmapWorkerTaskReference.get();
+    }
+}
