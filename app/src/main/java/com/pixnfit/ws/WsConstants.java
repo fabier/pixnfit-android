71533ccd0d5e98a42e6281384263ba1ba12d487c
Pierre FABIER <pierre.fabier@gmail.com>
2016-03-30 13:33:23 +0200

    Gestion dans le cas où le développeur se trouve à la serre

--- app/src/main/java/com/pixnfit/ws/WsConstants.java
+++ app/src/main/java/com/pixnfit/ws/WsConstants.java
@@ -6,9 +6,12 @@ package com.pixnfit.ws;
 public interface WsConstants {
 
     // Switch permettant de configurer pour la prod ou pour le dev local
-    boolean DEV_MODE = false;
+    boolean DEV_MODE = true;
+    boolean AT_HOME = false;
+    String IP_LASERRE = "192.168.78.204";
+    String IP_HOME = "192.168.1.128";
 
     // Production Configuration
-    String HOSTNAME = DEV_MODE ? "192.168.1.128" : "pixnfit.com";
+    String HOSTNAME = DEV_MODE ? AT_HOME ? IP_HOME : IP_LASERRE : "pixnfit.com";
     String BASE_URL = DEV_MODE ? "http://" + HOSTNAME + ":8080/pixnfit/api/v1" : "http://" + HOSTNAME + "/api/v1";
 }
