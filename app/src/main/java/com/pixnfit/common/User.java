599e8a5bcf9f56b8b460a2a2a044fcd1ca802088
Pierre FABIER <pierre.fabier@gmail.com>
2016-03-31 16:08:56 +0200

    Ajout des autres propriétés de User

--- app/src/main/java/com/pixnfit/common/User.java
+++ app/src/main/java/com/pixnfit/common/User.java
@@ -1,9 +1,23 @@
 package com.pixnfit.common;
 
+import java.util.Date;
+
 /**
  * Created by fabier on 16/02/16.
  */
 public class User extends BaseEntity {
     public String username;
     public Image image;
+    public String description;
+    public BodyType bodyType;
+    public Gender gender;
+    public Date birthdate;
+    public Integer height;
+    public Integer weight;
+    public Country country;
+    public Language language;
+    public Integer points;
+    public Integer postCount;
+    public Integer followersCount;
+    public Integer followedCount;
 }
