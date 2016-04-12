b1c7cef66023dc7a8e44fcc9bc38ae3febcf7d29
Pierre FABIER <pierre.fabier@gmail.com>
2016-04-12 20:01:25 +0200

    Possibilité de voir les followers d'un utilisateur et les personnes que l'utilisateur suit

--- app/src/main/java/com/pixnfit/fragment/ProfilePixFragment.java
+++ app/src/main/java/com/pixnfit/fragment/ProfilePixFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.pixnfit.R;
 import com.pixnfit.adapter.PostListAdapter;
@@ -16,6 +18,7 @@ import com.pixnfit.common.User;
 import com.pixnfit.utils.ThreadPools;
 import com.pixnfit.ws.GetUserPostAsyncTask;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.ListUtils;
 
 import java.util.List;
@@ -28,6 +31,9 @@ public class ProfilePixFragment extends Fragment {
     private User user;
     private GridView gridView;
     private PostListAdapter postListAdapter;
+    private LinearLayout pleaseWaitLayout;
+    private LinearLayout noResultsLayout;
+    private TextView noResultsTextView;
 
     @Nullable
     @Override
@@ -35,6 +41,15 @@ public class ProfilePixFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_profile_pix, container, false);
 
         gridView = (GridView) rootView.findViewById(R.id.gridView);
+        pleaseWaitLayout = (LinearLayout) rootView.findViewById(R.id.pleaseWaitLayout);
+        noResultsLayout = (LinearLayout) rootView.findViewById(R.id.noResultsLayout);
+        noResultsTextView = (TextView) rootView.findViewById(R.id.noResultsTextView);
+        noResultsTextView.setText(R.string.this_user_hasnt_posted_yet);
+
+        gridView.setVisibility(View.GONE);
+        pleaseWaitLayout.setVisibility(View.GONE);
+        noResultsLayout.setVisibility(View.GONE);
+
         postListAdapter = new PostListAdapter(getActivity());
         postListAdapter.setPostImagePlaceHolder(BitmapFactory.decodeResource(getResources(), R.drawable.camera_transparent));
         gridView.setAdapter(postListAdapter);
@@ -42,13 +57,23 @@ public class ProfilePixFragment extends Fragment {
         return rootView;
     }
 
-    private void refreshPosts() {
-        if (postListAdapter != null) {
+    private void refresh() {
+        if (postListAdapter != null && user != null) {
             postListAdapter.setPosts(null);
+
+            // On montre qu'on est en train de charger des données...
+            gridView.setVisibility(View.GONE);
+            pleaseWaitLayout.setVisibility(View.VISIBLE);
+            noResultsLayout.setVisibility(View.GONE);
+
             GetUserPostAsyncTask getUserPostAsyncTask = new GetUserPostAsyncTask(getActivity()) {
                 @Override
                 protected void onPostExecute(List<Post> posts) {
                     super.onPostExecute(posts);
+
+                    // Recherche terminée...
+                    pleaseWaitLayout.setVisibility(View.GONE);
+
                     if (!isCancelled()) {
                         if (ListUtils.isEqualList(posts, postListAdapter.getPosts())) {
                             // Les deux listes sont identiques, on ne change pas la valeur
@@ -56,6 +81,17 @@ public class ProfilePixFragment extends Fragment {
                             postListAdapter.addPostsUnique(posts);
                             postListAdapter.notifyDataSetChanged();
                         }
+
+                        if (CollectionUtils.isEmpty(posts)) {
+                            // On va afficher qu'il n'y a pas de résultats
+                            noResultsLayout.setVisibility(View.VISIBLE);
+                        } else {
+                            // Il y a des résultats...
+                            gridView.setVisibility(View.VISIBLE);
+                        }
+                    } else {
+                        // On a arrêté la recherche...
+                        noResultsLayout.setVisibility(View.GONE);
                     }
                 }
             };
@@ -65,20 +101,13 @@ public class ProfilePixFragment extends Fragment {
 
     public void setUser(User user) {
         this.user = user;
-        updateView();
+        refresh();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        updateView();
-    }
-
-    private void updateView() {
-        if (user == null) {
-        } else {
-            refreshPosts();
-        }
+        refresh();
     }
 
     public User getUser() {
