2f590e3381f254d6c2e63b8402ebc49fe7b5088f
Pierre FABIER <pierre.fabier@gmail.com>
2016-04-12 23:35:02 +0200

    Ajout de UserMe

---
+++ app/src/main/java/com/pixnfit/common/UserMe.java
@@ -0,0 +1,16 @@
+package com.pixnfit.common;
+
+/**
+ * Created by fabier on 12/04/16.
+ */
+public class UserMe {
+    /**
+     * True if me follows this user
+     */
+    public Boolean meFollows;
+
+    /**
+     * True if me is followed by this user
+     */
+    public Boolean meIsFollowed;
+}
