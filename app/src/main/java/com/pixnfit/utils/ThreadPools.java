44a6e83b86fb882abe690d64cb11a0d0a9b8edec
Pierre FABIER <pierre.fabier@gmail.com>
2016-04-15 13:17:10 +0200

    Diminution du nombre de threads pour charger les images (4 => 2)

--- app/src/main/java/com/pixnfit/utils/ThreadPools.java
+++ app/src/main/java/com/pixnfit/utils/ThreadPools.java
@@ -7,7 +7,7 @@ import java.util.concurrent.Executors;
  * Created by fabier on 28/02/16.
  */
 public class ThreadPools {
-    public static final Executor IMAGELOAD_THREADPOOL = Executors.newFixedThreadPool(4);
+    public static final Executor IMAGELOAD_THREADPOOL = Executors.newFixedThreadPool(2);
     public static final Executor METADATA_THREADPOOL = Executors.newFixedThreadPool(4);
     public static final Executor POST_THREADPOOL = Executors.newFixedThreadPool(4);
 }
