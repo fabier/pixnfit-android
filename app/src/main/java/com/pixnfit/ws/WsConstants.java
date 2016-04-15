0958a99e6fb3e1ac9a0560755fa76d9b9827aa09
Pierre FABIER <pierre.fabier@gmail.com>
2016-04-15 10:04:49 +0200

    Meilleure gestion du mode debug

--- app/src/main/java/com/pixnfit/ws/WsConstants.java
+++ app/src/main/java/com/pixnfit/ws/WsConstants.java
@@ -1,18 +1,20 @@
 package com.pixnfit.ws;
 
+
+import com.pixnfit.BuildConfig;
+
 /**
  * Created by fabier on 16/02/16.
  */
 public interface WsConstants {
 
     // Switch permettant de configurer pour la prod ou pour le dev local
-    boolean DEV_MODE = false;
     boolean AT_HOME = true;
     String LASERRE = "192.168.78.204";
     String HOME = "192.168.1.128";
     String PROD = "pixnfit.com";
 
     // Production Configuration
-    String HOSTNAME = DEV_MODE ? AT_HOME ? HOME : LASERRE : PROD;
-    String BASE_URL = DEV_MODE ? "http://" + HOSTNAME + ":8080/pixnfit/api/v1" : "http://" + HOSTNAME + "/api/v1";
+    String HOSTNAME = BuildConfig.DEBUG_MODE ? (AT_HOME ? HOME : LASERRE) : PROD;
+    String BASE_URL = BuildConfig.DEBUG_MODE ? "http://" + HOSTNAME + ":8080/pixnfit/api/v1" : "http://" + HOSTNAME + "/api/v1";
 }
